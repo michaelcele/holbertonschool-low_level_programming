ede11064314443f1867999da2a5f5262c7854f97
michaelcele <celeita.michael02@gmail.com>
2021-06-17 20:29:52 +0000

    9-Unix_is_basically

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,8 @@
  */
 int main(void)
 {
-	char Myfirst[] = "and that piece of art is useful\" - \
-			Dora Korpar, 2015-10-19\n";
+	char Myfirst[] = "and that piece of art is useful\" - Dora Korpar,\
+2015-10-19\n";
 	int i = 0;
 
 	while (Myfirst[i])
