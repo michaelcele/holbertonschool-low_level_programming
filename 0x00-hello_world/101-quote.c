560388228b162266122118978800d8f996c14677
michaelcele <celeita.michael02@gmail.com>
2021-06-17 20:05:44 +0000

    9-Unix is basically

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - prints string
+ * Return: 0
+ */
+int main(void)
+{
+	char Myfirst[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int i = 0;
+
+	while (Myfirst[i])
+	{
+		putchar(Myfirst[i++]);
+	}
+
+	return (1);
+}
