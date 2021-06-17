407714327d87b69b9d868ee55dd3229a76ef5d98
michaelcele <celeita.michael02@gmail.com>
2021-06-17 18:15:52 +0000

    4-Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - input puts
+ * Return: 0
+ */
+int main(void)
+{
+	char Myfirst[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Myfirst);
+
+	return(0);
+}
