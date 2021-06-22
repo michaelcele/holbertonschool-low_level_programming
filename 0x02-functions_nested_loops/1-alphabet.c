06f1bac3f4cb455b220861641fba10f7c6ba1598
michaelcele <celeita.michael02@gmail.com>
2021-06-22 14:44:12 +0000

    1-Alphabet_game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_alphabet- Print to alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+}
