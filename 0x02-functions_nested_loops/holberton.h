d4564e02677d10b80c46571284f4f07a200367bb
michaelcele <celeita.michael02@gmail.com>
2021-06-22 17:25:18 +0000

    file header

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -2,4 +2,12 @@
 #define HOLBERTON_H
 /*function putchar holberton*/
 int _putchar(char c);
+/*Print to alphabet*/
+void print_alphabet(void);
+/*Print to alphabet x10*/
+void print_alphabet_x10(void);
+/*Return 1 if is c is lowercase*/
+int _islower(int c);
+/*Return 1 if is c is lowercase or uppercase*/
+int _isalpha(int c);
 #endif
