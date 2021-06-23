6f525959fdc123c4e3434969db864c5ad7baff2b
michaelcele <celeita.michael02@gmail.com>
2021-06-23 18:17:38 +0000

    0-Multiple_mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/*
+ * Main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(NULL));
+	n = rand();
+
+	return(0);
+			
+
+}
---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,5 @@
+#ifndef HIOLBERONT_H
+#define HOLBERTON_H
+/*print numbers of n to 98*/
+void positive_or_negative(void);
+#endif
