2f9d8aae84b98e7138a4387cace41e421ee5504c
michaelcele <celeita.michael02@gmail.com>
2021-06-25 02:44:33 +0000

    4-Print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_most_numbers  - print numbers of 0 to 9 excluding the 2 and 4
+ *
+ */
+void print_most_numbers(void)
+{
+
+	char i[] = "01356789";
+	int j = 0;
+
+	while (i[j])
+	{
+		_putchar(i[j]);
+	}
+	_putchar('\n');
+}
