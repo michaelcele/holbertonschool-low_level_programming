f1cbc4031b769109414d6ad578365ee3d6283674
michaelcele <celeita.michael02@gmail.com>
2021-07-05 23:37:25 -0500

    7-print_chessboard

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -12,4 +12,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 /*locates a substring.*/
 char *_strstr(char *haystack, char *needle);
+/*prints the chessboard.*/
+void print_chessboard(char (*a)[8]);
 #endif
