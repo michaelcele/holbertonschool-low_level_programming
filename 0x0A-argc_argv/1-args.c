371a6de5fe062bb769591a553c8e66f50fb02ee2
michaelcele <celeita.michael02@gmail.com>
2021-07-08 13:53:06 -0500

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: integer size of argv
+ * @argv: array comand line arguments.
+ *
+ * Return: Always 0
+ */
+int main(int argc, char* argv[])
+{
+	int i = 0;
+
+	
+	while (i != argc - 1)
+	{
+		i++;
+	}
+	printf("%d\n", i);
+	argv[0] = argv[i];
+
+	return (0);	
+}
