2d07bdf8d8658e3a76e8a1f9e72126b9bfb86d47
michaelcele <celeita.michael02@gmail.com>
2021-07-17 19:15:33 -0500

    2-str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,9 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 
 	if (s1 == NULL)
-		s1 = (" ");
+		s1 = ("");
 	if (s2 == NULL)
-		s2 = (" ");
+		s2 = ("");
 	str = (char *)malloc(strlen(s1) + strlen(s2) + 1);
 
 	if (!str)
